96560c60acedc44d53c8073470d2fce823985cf3
DIBYAJYOTI BHUYAN <bhuyand143@gmail.com>
2023-02-11 16:04:18 +0530

    Update SignUp.js

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -40,20 +40,20 @@ const SignUp = () => {
       <form onSubmit={handleSubmit}>
         <div className="mb-3">
           <label htmlFor="name" className="form-label">Name</label>
-          <input type="text" className="form-control" name='name' id="name" onChange={onChange} />
+          <input type="text" className="form-control" name='name' id="name" onChange={onChange} required minLength={3} />
         </div>
         <div className="mb-3">
           <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-          <input type="email" className="form-control" name='email' id="email" aria-describedby="emailHelp" onChange={onChange} />
+          <input type="email" className="form-control" name='email' id="email" aria-describedby="emailHelp" onChange={onChange} required/>
           <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
         </div>
         <div className="mb-3">
           <label htmlFor="password" className="form-label">Password</label>
-          <input type="password" name='password' className="form-control" id="password" onChange={onChange} />
+          <input type="password" name='password' className="form-control" id="password" onChange={onChange} required minLength={5}/>
         </div>
         <div className="mb-3">
           <label htmlFor="cpassword" className="form-label">Confirm Password</label>
-          <input type="password" name='cpassword' className="form-control" id="cpassword" onChange={onChange} />
+          <input type="password" name='cpassword' className="form-control" id="cpassword" onChange={onChange} required minLength={5} />
         </div>
         <button type="submit" className="btn btn-primary">Submit</button>
       </form>
